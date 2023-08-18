b567bd6b7a006ef1c69509b4af60273cba76f42d
jaehyeong_ <pshong2012@gmail.com>
2023-08-18 15:17:08 +0900

    캘린더 일정등록

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -9,6 +9,10 @@ class Calendar extends Component {
                 <FullCalendar
                     defaultView="dayGridMonth"
                     plugins={[dayGridPlugin]}
+                    events={[
+                        { title: '방배동 신라빌딩 작업', date: '2023-08-18'},
+                        { title: '사당동 삼송빌딩 작업', date: '2023-08-31'}
+                    ]}
                 />
             </div>
         );
