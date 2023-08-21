1cdf198750fab1c8186d3d6a6a490d766153d578
heski0709 <heski0709@gmail>
2023-08-21 09:58:36 +0900

    [style] 이름수정

---
+++ src/pages/attendance/main/AttendanceModal.js
@@ -0,0 +1,22 @@
+import React from "react";
+import popup from './popup.module.css';
+
+const AttendanceModal = ({ isOpen, workType }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal">
+            <div className="modal-content">
+                <div className={popup['work-popup']}>
+                    <img className={popup.circle} src='/common/images/Ellipse.svg' alt='' />
+                    <img className={popup.check} src='/common/images/check.png' alt="" />
+                    <span className={popup['popup-text']}>
+                    {(workType === '출근') ? '출근 처리가 완료되었습니다.' : '퇴근 처리가 완료되었습니다.'}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AttendanceModal;
