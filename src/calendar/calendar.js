ba261ae57d1e9e33b0509af83a2acdb9be7e39d3
jaehyeong_ <pshong2012@gmail.com>
2023-08-23 13:06:00 +0900

    캘린더데이터값

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import './calendar.css';    
 
 class Calendar extends Component {
+    state = {
+        events: []
+    };
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get('http://localhost:8080/api/v1/calendar');
+            const eventData = response.data.data.map(e => ({
+                title: e.title,
+                start : e.start_date,
+                end : e.end_date
+            }));
+            this.setState({ events: eventData });
+        } catch(error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
     render() {
         return (
             <div className="App">
                 <FullCalendar
                     defaultView="dayGridMonth"
                     plugins={[dayGridPlugin]}
-                    events={[
-                        { title: '방배동 신라빌딩 작업', date: '2023-08-18'},
-                        { title: '사당동 삼송빌딩 작업', date: '2023-08-31'}
-                    ]}
+                    {...this.state}
                 />
             </div>
         );
