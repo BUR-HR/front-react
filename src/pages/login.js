e6223382a61426280f98b132a334357ad4563317
yuju <zzgmlo3o@naver.com>
2023-08-23 10:04:41 +0900

    로그인

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,25 +1,68 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import '../css/login.css'
 
 export const Login = () => {
+    const navigate = useNavigate();
+
+    const [form, setForm] =  useState({
+        empNo: '',
+        password: ''
+    });
+
+    const onChangeHandler = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+    
+
+        // API 호출을 위한 로직 
+        try {
+            const response = await fetch('/api/login', {
+                method: 'POST',
+                headres: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({
+                    empNo: form.empNo,
+                    password: form.password
+                })
+            });
+
+            if(response.ok) {
+                //로그인 성공 시 메인페이지로 이동 
+                navigate("/", { replace: true });
+            } else {
+                //로그인 실패 시 처리 
+                console.log("로그인 실패");
+            }
+        } catch (error) {
+            console.error("API 호출 에러", error);
+        }
+    };
+
+
     return (
         <div className="login-body">
             <div className="loginlogo">
                 <img src="/common/images/logo.png" alt="logo" />
             </div>
-            <div className="login-form">
-                <form>
-                    <div className="remember">
-                        <input type="checkbox" id="remember" name="remember" />
-                        <label htmlFor="remember">사원번호 저장</label>
-                    </div>
 
-                    <label htmlFor="username"></label>
-                    <input
+            <div className="login-form">
+                <form onSubmit={onSubmitHandler}>
+                    <label htmlFor="empNo"></label>
+                    <input  
                         type="text"
-                        id="username"
-                        name="username"
+                        id="empNo"
+                        name="empNo"
                         placeholder="사원번호"
+                        value={form.empNo}
+                        onChange={onChangeHandler}
                     />
                     <br />
 
@@ -29,6 +72,8 @@ export const Login = () => {
                         id="password"
                         name="password"
                         placeholder="비밀번호"
+                        value={form.password}
+                        onChange={onChangeHandler}
                     />
 
                     <button type="submit">로그인</button>
