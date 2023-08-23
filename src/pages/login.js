6b1de2ea70b29853bc337ccd5cea44013c9ad20e
yuju <zzgmlo3o@naver.com>
2023-08-23 14:37:10 +0900

    로그인에러문구

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,12 @@ import '../css/login.css'
 import { login } from "../api/service";
 
 export const Login = () => {
+    const [empNo, setEmpNo] = useState(""); 
+    const [password, setPassword] = useState(""); 
+    const [showEmpNoError, setShowEmpNoError] = useState(false);
+    const [showPasswordError, setShowPasswordError] = useState(false);
+    const navigate = useNavigate();
+
     const onSubmit = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target)
@@ -13,9 +19,26 @@ export const Login = () => {
             data[`${pair[0]}`] = pair[1];
         }
 
+        if (empNo === "") {
+            setShowEmpNoError(true);
+        } else {
+            setShowEmpNoError(false);
+        }
+
+        if (password === "") {
+            setShowPasswordError(true);
+        } else {
+            setShowPasswordError(false);
+        }
+
+        if (empNo === "" || password === "") {
+            return; 
+        }
+
         console.log(data);
         login(data);
-    }
+
+    };
 
     return (
         <div className="login-body">
@@ -36,20 +59,31 @@ export const Login = () => {
                         id="username"
                         name="empNo"
                         placeholder="사원번호"
-                        value={form.empNo}
-                        onChange={onChangeHandler}
+                        className={showEmpNoError ? "error" : ""}
+                        value={empNo}
+                        onChange={(e) => {
+                            setEmpNo(e.target.value);
+                            setShowEmpNoError(false); 
+                        }}
                     />
-                    <br />
+                        {showEmpNoError && <span className="error-text">사원번호를 입력해주세요.</span>}
+                        <br />
 
-                    <label htmlFor="password"></label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="employeePassword"
-                        placeholder="비밀번호"
-                        value={form.password}
-                        onChange={onChangeHandler}
-                    />
+                        <label htmlFor="password"></label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="employeePassword"
+                            placeholder="비밀번호"
+                            className={showPasswordError ? "error" : ""}
+                            value={password}
+                            onChange={(e) => {
+                                setPassword(e.target.value);
+                                setShowPasswordError(false);
+                            }}
+                        />
+                        {showPasswordError && <span className="error-text">비밀번호를 입력해주세요.</span>}
+                        
 
                     <button type="submit">로그인</button>
                 </form>
