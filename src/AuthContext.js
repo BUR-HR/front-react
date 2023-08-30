ee9803ec94050a27a6598b7745e0376b45d5c774
heski0709 <heski0709@gmail>
2023-08-30 15:19:09 +0900

    로그인 추가

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -17,7 +17,8 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const token = localStorage.getItem("ACCESS_TOKEN");
-    const [auth, setAuth] = useState(null)
+    const [auth, setAuth] = useState(null);
+    const [loading, setLoading] = useState(true);
     const [attendanceState, attendanceDispatch] = useReducer(
         reducer,
         initialState
@@ -31,23 +32,26 @@ export const AuthProvider = ({ children }) => {
             );
             setAuth(JSON.parse(base64.decode(payload)).auth);
 
-            call("/api/v1/attendance/status", "POST", null).then((data) => {
-                if (data.status === 404) {
-                    attendanceDispatch({
-                        type: SET_ATTENDANCE_TYPE,
-                        payload: "출근",
-                    });
-                } else {
-                    attendanceDispatch({
-                        type: SET_ATTENDANCE_TYPE,
-                        payload: data.attendanceType,
-                    });
-                    attendanceDispatch({
-                        type: SET_START_TIME,
-                        payload: data.startDateTime
-                    })
-                }
-            });
+            call("/api/v1/attendance/status", "POST", null)
+                .then((data) => {
+                    if (data.status === 204) {
+                        attendanceDispatch({
+                            type: SET_ATTENDANCE_TYPE,
+                            payload: "출근",
+                        });
+                    } else {
+                        attendanceDispatch({
+                            type: SET_ATTENDANCE_TYPE,
+                            payload: data.attendanceType,
+                        });
+                        attendanceDispatch({
+                            type: SET_START_TIME,
+                            payload: data.startDateTime,
+                        });
+                    }
+
+                })
+                .catch(() => {});
         }
     }, []);
 
